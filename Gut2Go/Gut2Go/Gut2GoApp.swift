7373235651b9ec95b8713032d2516369454dc6c9
Robert <60601627+Robert-ctrlc@users.noreply.github.com>
2024-11-02 16:02:36 +0000

    Supabase Set up

---
+++ Gut2Go/Gut2Go/Gut2GoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gut2GoApp.swift
+//  Gut2Go
+//
+//  Created by Robert Manolache on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Gut2GoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gut2Go/Gut2GoTests/Gut2GoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gut2GoTests.swift
+//  Gut2GoTests
+//
+//  Created by Robert Manolache on 02/11/2024.
+//
+
+import Testing
+@testable import Gut2Go
+
+struct Gut2GoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
